bdc9edc2be49f8b89bc06dc297de47642c32fed9
Daniel Kogan <daniel@techhacks.nyc>
2020-04-09 07:55:52 -0400

    did i forget to add procfile lol

--- bot.py
+++ bot.py
@@ -1,23 +1,66 @@
 # bot.py
 import os
 
-import discord,random
+import discord, random
 from dotenv import load_dotenv
+from discord import Member
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 from discord.ext import commands
-bot = commands.Bot(command_prefix='!')
+from discord.ext.commands import has_permissions, MissingPermissions
+
+bot = commands.Bot(command_prefix='!', description='owo i sure do L-O-V-E programming')
 
 client = discord.Client()
 
+
 @bot.command(name='create-team')
-async def create_team(ctx):
+async def create_team(ctx, role):
     guild = ctx.guild
-    await guild.create_role(name="role name")
-    role = discord.utils.get(ctx.guild.roles, name="role name")
+    await guild.create_role(name=role)
+    role = discord.utils.get(ctx.guild.roles, name=role)
+    user = ctx.message.author
+    try:
+        await user.add_roles(role)
+    except discord.errors.Forbidden:
+        ctx.message('Sorry, I can\'t let you do that. The execs are a far greater power than I, and I worship them as '
+                    'gods')
+
+
+@bot.command(name='join-team')
+async def create_team(ctx, role):
+    guild = ctx.guild
+    role = discord.utils.get(guild.roles, name=role)
     user = ctx.message.author
     await user.add_roles(role)
+
+
+@bot.command(name='kick', pass_context=True,)
+@has_permissions(ban_members=True)
+async def _kick(ctx, member: Member):
+    await ctx.kick(member)
+    await ctx.channel.send('{} has been kicked'.format(ctx.member))
+
+
+@_kick.error
+async def kick_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        text = "I can\'t let you do that {}".format(ctx.message.author)
+        await ctx.send(ctx.message.channel, text)
+
+
+@bot.command(name='ban', pass_context=True)
+@has_permissions(ban_members=True)
+async def _ban(ctx, member: Member):
+    await ctx.ban(member)
+
+
+@_ban.error
+async def kick_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        text = "I can\'t let you do that {}".format(ctx.message.author)
+        await ctx.channel.send(text)
 
 
 @bot.command(name='99')
@@ -32,8 +75,6 @@ async def nine_nine(ctx):
     ]
     response = random.choice(brooklyn_99_quotes)
     await ctx.channel.send(response)
-
-
 
 
 bot.run(TOKEN)
