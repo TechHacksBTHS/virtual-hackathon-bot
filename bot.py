4c7aa9812a3f63c471349c774d81ff20409bd13e
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-11-22 00:35:41 -0500

    autosave

--- bot.py
+++ bot.py
@@ -115,6 +115,7 @@ async def on_raw_reaction_add(payload):
     message_id = 779426797044891688
     Technight2020 = guild.get_role(779364367145500702)
     user = bot.get_user(payload.user_id)
+    await channel.send(payload.message_id)
     if payload.emoji == "✋" and payload.message_id == message_id:
         print(Technight2020)
         await user.add_roles(Technight2020)
