33ad7f17bfa08e12bf190fd00e77054665fb6a88
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-11-21 23:59:26 -0500

    autosave

--- bot.py
+++ bot.py
@@ -122,6 +122,7 @@ async def on_raw_reaction_remove(payload):
     channel = bot.get_channel(payload.channel_id)
     # message_id = 779426797044891688
     message_id = 779930304251953152
+    await channel.send(payload)
     Technight2020 = bot.get_role(779364367145500702)
     if payload.emoji == "✋" and payload.message_id == message_id:
         await user.remove_roles(Technight2020)
