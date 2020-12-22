ae81d3c04003070edb3954dbbd7e0096021c29f2
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-12-22 12:07:35 -0500

    iForgotToTabb

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,7 +14,7 @@ class Fun(commands.Cog):
     async def on_message(self, message):
         if (len(message.attachments) > 0 or 'https://cdn.discordapp.com/attachments/' in message.content) and message.channel.id == 697531624492892207:
             await message.add_reaction('<:upvote:776161705960931399>')
-        await message.add_reaction('<:downvote:776162465842200617>')
+            await message.add_reaction('<:downvote:776162465842200617>')
 
 
 
