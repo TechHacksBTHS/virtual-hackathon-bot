14709ecc4aefb756e59461dca71835055a82d964
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-04-15 23:23:02 -0400

    attachm

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,12 @@
 import discord, random
 from discord.ext import commands
 
+def attachm(message):
+    if (len(message.attachments) > 0 or 'https://' in message.content):
+        return True
+    else:
+        return False
+
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,7 +18,7 @@ class Fun(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if (len(message.attachments) > 0 or 'https://cdn.discordapp.com/attachments/' in message.content) and message.channel.id == 697531624492892207:
+        if attachm(message):
             await message.add_reaction('<:upvote:776161705960931399>')
             await message.add_reaction('<:downvote:776162465842200617>')
 
